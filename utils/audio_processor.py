ea66ce60714c0fae87e8a0eee47fd459433f4446
Sahil Omanwar <omanwarsahil@gmail.com>
2026-08-08 01:46:25 +0530

    Add function to convert files to wav format and set channel

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -3,9 +3,10 @@ from pydub import AudioSegment
 import os
 
 #Create a folder to save
-DOWNLOAD_DIR='downloads'
-os.makedirs(DOWNLOAD_DIR,exist_ok=True)
-
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DOWNLOAD_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, '..', 'downloads'))
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+#youtube videos download
 
 def download_youtube_audio(url:str)->str:
     output_path=os.path.join(DOWNLOAD_DIR,"%(title)s.%(ext)s")
@@ -25,3 +26,17 @@ def download_youtube_audio(url:str)->str:
         info=ydl.extract_info(url,download=True)
         filename=ydl.prepare_filename(info).replace(".webm",".wav").replace(".m4a",".wav")
     return filename
+
+data=download_youtube_audio("https://youtu.be/BHihkRwisbE?si=qrIdQ7WxDqahKbad")
+
+
+
+#conveting any audio/video file to wav format using pydub
+def convert_to_wav(input_path:str)->str:
+    output_path=os.path.splitext(input_path)[0]+"_converted.wav"
+    audio=AudioSegment.from_file(input_path) #deteh audio type 
+    audio=audio.set_channels(1).set_frame_rate(16000) #set_channel->monoaudio ,16khz->
+    audio.export(output_path,format="wav")
+    return output_path
+
+print(convert_to_wav(data))
